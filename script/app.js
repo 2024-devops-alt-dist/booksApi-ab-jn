6d39f39f8366a13d86916bb6b38d659220f4f706
jimni6 <nijimmy@outlook.fr>
2024-10-24 11:57:48 +0200

    feat: user can search a book on landing page

--- script/app.js
+++ script/app.js
@@ -1,15 +1,22 @@
 const API_KEY = 'AIzaSyAZSXaq2jKWgfLA5ONNg7YCgFiKq5CQihQ'; // Replace with your Google Books API key
 
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click",searchBooks);
+
+let results = 10;
+
+
 async function searchBooks() {
     const query = document.getElementById('search-bar').value;
     if (!query) {
         alert('Please enter a search query');
         return;
-    }
-    
+    }    
     try {
-        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`);
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${results}&key=${API_KEY}`);
         const data = await response.json();
+        console.log(data);
         displayBooks(data.items);
     } catch (error) {
         console.error('Error fetching books:', error);
@@ -18,10 +25,12 @@ async function searchBooks() {
 
 function displayBooks(books) {
     const booksList = document.getElementById('books-list');
-    booksList.innerHTML = ''; // Clear previous results
+    // booksList.innerHTML = ''; // Clear previous results
 
     books.forEach(book => {
         const bookElement = document.createElement('div');
+        bookElement.classList.add('myClass');
+
         const title = book.volumeInfo.title || 'No title available';
         const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'No authors available';
         const description = book.volumeInfo.description || 'No description available';
@@ -37,4 +46,3 @@ function displayBooks(books) {
     });
 }
 
-// export default searchBooks();
